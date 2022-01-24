5b1f1afa7b359b6347d8004eec2cb396ce5976cb
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-24 15:52:27 +0100

    prepeared classes added getNumberOfOrdersByStatus implemented

---
+++ week14-labs-task/src/main/java/day01/OrderService.java
@@ -0,0 +1,23 @@
+package day01;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OrderService {
+
+    private List<Order> orders = new ArrayList<>();
+
+    public void saveOrder(Order order) {
+        orders.add(order);
+    }
+
+    public long getNumberOfOrdersByStatus(String status) {
+        return orders.stream()
+                .filter(order -> order.getStatus().equals(status))
+                .count();
+    }
+}
