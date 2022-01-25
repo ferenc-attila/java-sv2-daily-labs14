515b9d7460db00af2c4ed0126e4777bce4ba838d
ferenc-attila <attila.ferenc.80@gmail.com>
2022-01-25 15:18:34 +0100

    day02 solved

---
+++ week14-labs-task/src/main/java/day02/MovieService.java
@@ -0,0 +1,28 @@
+package day02;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public class MovieService {
+
+    private List<Movie> movies = new ArrayList<>();
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public List<Movie> findMovieByActor(String actor) {
+        return movies.stream()
+                .filter(movie -> movie.getActors().stream().anyMatch(actual -> actual.equals(actor)))
+                .toList();
+    }
+
+    public int getLongestMovieLength() {
+        return movies.stream()
+                .max(Comparator.comparingInt(Movie::getLength))
+                .orElseThrow(() -> new IllegalStateException("Empty list.")).getLength();
+    }
+}
